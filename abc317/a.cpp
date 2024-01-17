a1404771d22e99c512a61aca1f4095f2d06b428d
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-01-17 18:59:36 +0900

    feat:abc317-a

---
+++ abc317/a.cpp
@@ -0,0 +1,26 @@
+//abc317-a
+#include<stdio.h>
+
+int main(void){
+    int n,h,x;
+    int p[101];
+    int q,r;
+    int i,ans;
+    scanf("%d %d %d",&n,&h,&x);
+    for(i=0;i<n;i++){
+        scanf("%d",&p[i]);
+    }
+    q=999+h;
+    r=0;
+    for(i=0;i<n;i++){
+        ans=0;
+        ans=h+p[i];
+        if(x<=ans&&ans<=q){
+            q=ans;
+            r=i+1;
+        }
+    }
+    printf("%d\n",r);
+
+    return 0;
+}
