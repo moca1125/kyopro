aa325128b6e2d4b5e726344813c1f16d430fa0f3
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-01-25 23:18:50 +0900

    abc309-a

---
+++ abc/309/a.cpp
@@ -0,0 +1,15 @@
+//2024/01/25
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a>>b;
+    if((a==b-1&&!(a%3==0))){
+        cout<<"Yes"<<endl;
+    }
+    else {
+        cout<<"No"<<endl;
+    }
+
+}
