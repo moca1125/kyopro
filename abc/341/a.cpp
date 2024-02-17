a8cf04fff2d0327ed8c7a67dd624af2600938643
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-02-17 22:27:28 +0900

    abc341

---
+++ abc/341/a.cpp
@@ -0,0 +1,15 @@
+// 2024/02/17
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "10";
+    }
+    cout << "1" << endl;
+}
---
+++ abc/341/b.cpp
@@ -0,0 +1,21 @@
+// 2024/02/17
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> s(n), t(n);
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cin >> a.at(i);
+    }
+    for (i = 0; i < n; i++)
+    {
+        cin >> s.at(i) >> t.at(i);
+    }
+}
