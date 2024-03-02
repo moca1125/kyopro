1ddb3e3b7c2d2ffdb087657fe64a42540fc14c8e
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-03 01:23:51 +0900

    abc343-c_exAC

--- abc/343/c.cpp
+++ abc/343/c.cpp
@@ -3,6 +3,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool check(long long int n);
+
 int main()
 {
+    long long int n;
+    cin >> n;
+    long long ans = 0;
+    long long int i;
+    for (i = 1; i * i * i <= n; i++)
+    {
+        if (check(i * i * i)) // check関数(引数i*i*i)がtrueならif文もtrue
+        {
+            ans = i * i * i; // 回文だったらansに代入
+        }
+    }
+    cout << ans << endl;
+}
+
+bool check(long long int n)
+{
+    string s = to_string(n);     // int型をstring型に変換
+    string t = s;                // sをtにコピー
+    reverse(t.begin(), t.end()); // sを逆順にする
+    return s == t;               // s==tだったらtrueが返る　回文かどうか判断
 }
