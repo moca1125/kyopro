43c0dfc2103795d37082d3f4ade9bff829e6a530
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-15 23:47:52 +0900

    abc285-a_exAC

---
+++ abc/285/a.cpp
@@ -0,0 +1,14 @@
+// 2024/03/15
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (b / 2 == a)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
