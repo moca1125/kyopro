7ac3a5e07a08afb22fd6b5e5bd8bb736cc7a9e79
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-16 19:29:37 +0900

    😊abc088-b_AC

---
+++ abc/088/b.cpp
@@ -0,0 +1,30 @@
+// 2024/03/16
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int alice = 0, bob = 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a.at(i);
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            alice += a.at(n - i - 1);
+        }
+        else
+        {
+            bob += a.at(n - i - 1);
+        }
+    }
+
+    cout << alice - bob << endl;
+}
