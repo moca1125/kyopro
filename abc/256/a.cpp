a1594bb120b96e703a29a521dac9310fd3d9a30c
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-18 21:15:28 +0900

    😊abc256-a_AC

---
+++ abc/256/a.cpp
@@ -0,0 +1,13 @@
+// 2024/03/18
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long int ans;
+    ans = pow(2, n);
+    cout << ans << endl;
+}
