cf7dd59f6b497231f403ed6dd7c525c6112ad698
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-18 21:23:42 +0900

    😊abc283-a_AC

---
+++ abc/283/a.cpp
@@ -0,0 +1,13 @@
+// 2024/03/18
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    long long int ans;
+    cin >> a >> b;
+    ans = pow(a, b);
+    cout << ans << endl;
+}
