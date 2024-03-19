18a14fcc41c659738ce1428fc83d30519e486389
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-19 22:16:26 +0900

    😊abc281-a_AC

---
+++ abc/281/a.cpp
@@ -0,0 +1,15 @@
+// 2024/03/19
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (0 <= n)
+    {
+        cout << n << endl;
+        n--;
+    }
+}
