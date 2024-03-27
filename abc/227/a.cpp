2c0b3f00341fbb4b04ba99d3076417439cbe4a42
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-27 23:56:07 +0900

    🥺abc251-a_exAC

--- abc/227/a.cpp
+++ abc/227/a.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     int n, k, a;
-    cin >> n >> k << a;
-    ins ans = (a + k - 1) % n;
+    cin >> n >> k >> a;
+    int ans = (a + k - 1) % n;
     if (ans == 0)
     {
         ans = n;
