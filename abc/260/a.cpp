585d10112e41e008e51df3fdc76518d2cec55546
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-03-29 23:25:53 +0900

    😊abc260-a_AC

---
+++ abc/260/a.cpp
@@ -0,0 +1,13 @@
+// 2024/03/29
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c - d) << endl;
+    cout << "Takahashi" << endl;
+    return 0;
+}
