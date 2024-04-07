705736b6c2513af659d172cec2955fb854e712b1
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-07 23:58:43 +0900

    😊abc262-a_AC

---
+++ abc/262/a.cpp
@@ -0,0 +1,29 @@
+// 2024/04/07
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int y;
+    cin >> y;
+    if (y % 4 == 2)
+    {
+        cout << y << endl;
+    }
+    else
+    {
+        if (y % 4 == 0)
+        {
+            cout << y + 2 << endl;
+        }
+        else if (y % 4 == 1)
+        {
+            cout << y + 1 << endl;
+        }
+        else if (y % 4 == 3)
+        {
+            cout << y + 3 << endl;
+        }
+    }
+}
