b9fb8cbb10157d10023238da2f223345302345b8
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-13 21:03:57 +0900

    abc307

---
+++ abc/307/b.cpp
@@ -0,0 +1,20 @@
+// 2024/04/12
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    vector<vector<要素の型>> 変数名(縦の要素数, vector<要素の型>(横の要素数));
+    bool ans = true;
+    for (auto &ai : s)
+    {
+        cin >> ai;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        }
+}
