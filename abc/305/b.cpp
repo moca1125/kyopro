a7dabf77bb68a04ef845f9c445054bd483c1d141
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-15 00:16:19 +0900

    😊abc305-a_AC

---
+++ abc/305/b.cpp
@@ -0,0 +1,32 @@
+// 2024/04/015
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char p, q;
+    cin >> p >> q;
+    int begin, end;
+    vector<int> s = {3, 1, 4, 1, 5, 9};
+
+    if (q < p)
+    {
+        int tmp;
+        tmp = p;
+        p = q;
+        q = tmp;
+        // 解説はswap(p,q);してる
+    }
+    else
+    {
+    }
+    begin = p - 'A';
+    end = q - 'A';
+    int ans = 0;
+    for (int i = begin; i < end; i++)
+    {
+        ans += s[i];
+    }
+    cout << ans << endl;
+}
