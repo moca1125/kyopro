45c3aa0d0273e72016a13a982e4fe43394ff1170
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-22 23:50:14 +0900

    😊abc226-a_AC

---
+++ abc/226/a.cpp
@@ -0,0 +1,13 @@
+// 2024/04/22
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x;
+    cin >> x;
+    int ans;
+    ans = (int)(x + 0.5);
+    cout << ans << endl;
+}
