831d73eab7f46c093990c95bacdcc252e19e4a44
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-28 02:20:02 +0900

    😭abc351-c_WA

--- abc/351/c.cpp
+++ abc/351/c.cpp
@@ -10,7 +10,6 @@ int main()
     cin >> n;
     vector<int> a(n);
     vector<long long> ans;
-    int num = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
@@ -18,31 +17,50 @@ int main()
 
     for (int i = 1; i < n; i++)
     {
-        ans.push_back(pow(2, a[i - 1]));
+        ans.push_back(a[i - 1]);
         while (1)
         {
             if (ans.size() == 1)
             {
-                cout << i << "a" << ans[ans.size() - 1] << endl;
+
                 break;
             }
             else if (ans[ans.size() - 2] == ans[ans.size() - 1])
             {
+                int f = ans[ans.size() - 1];
                 ans.pop_back();
                 ans.pop_back();
-                ans.push_back(ans[ans.size() - 1] * 2);
-                cout << i << "b" << ans[ans.size() - 1] << endl;
+                ans.push_back(f + 1);
             }
             else
             {
-                cout << i << "c" << ans[ans.size() - 1] << endl;
+
                 break;
             }
         }
+        if (i == n - 1)
+        {
+            ans.push_back(a[n - 1]);
+            while (1)
+            {
+                if (ans.size() == 1)
+                {
+                    break;
+                }
+                else if (ans[ans.size() - 2] == ans[ans.size() - 1])
+                {
+                    int f = ans[ans.size() - 1];
+                    ans.pop_back();
+                    ans.pop_back();
+                    ans.push_back(f * 2);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << endl;
-    }
+
     cout << ans.size() << endl;
 }
