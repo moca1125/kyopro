1c959d33db8f08d06ea028812ebfde63f372b473
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-04-30 22:24:22 +0900

    😊abc199-a_AC

---
+++ abc/199/a.cpp
@@ -0,0 +1,18 @@
+// 2024/04/20
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b, c;
+    cin >> a >> b >> c;
+    if (a * a + b * b < c * c)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" <, endl;
+    }
+}
