5b5008c84c9a0b571b0b627996da698b6ad31e38
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-04 01:24:51 +0900

    😊abc211-a_AC

---
+++ abc/211/a.cpp
@@ -0,0 +1,13 @@
+// 2024.05.03
+// 自力AC
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    double c = ((a - b) / 3) + b;
+    printf("%lf", c);
+}
