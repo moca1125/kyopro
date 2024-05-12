87420a9cd00fc347e104ba57943cbf6a554a6d81
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-12 23:54:01 +0900

    😊abc217-a_AC

---
+++ abc/217/a.cpp
@@ -0,0 +1,18 @@
+// 2024/05/12
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    if (s < t)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
