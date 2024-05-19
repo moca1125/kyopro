50522b7a70e2435c4992de673b7b949ed515d5d8
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-19 23:38:04 +0900

    😊abc215-a_AC

---
+++ abc/215/a.cpp
@@ -0,0 +1,18 @@
+// 2024/05/19
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (s == "Hello,World!")
+    {
+        cout << "AC" << endl;
+    }
+    else
+    {
+        cout << "WA" << endl;
+    }
+}
