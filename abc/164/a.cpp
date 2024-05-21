01bda20f6327a817be6e76a9fbe5912d2fefb15e
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-22 06:57:45 +0900

    😊abc164-a_AC

---
+++ abc/164/a.cpp
@@ -0,0 +1,14 @@
+// 2024/05/22
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int s, w;
+    cin >> s >> w;
+    if (s <= w)
+        cout << "unsafe" << endl;
+    else
+        cout << "safe" << endl;
+}
