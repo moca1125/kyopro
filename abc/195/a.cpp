4dd99bb0c897fb480916306dd171c234e7b603b2
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-22 06:52:00 +0900

    😊abc195-a_AC

---
+++ abc/195/a.cpp
@@ -0,0 +1,14 @@
+// 2024/05/22
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, h;
+    cin >> m >> h;
+    if (h % m == 0)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
