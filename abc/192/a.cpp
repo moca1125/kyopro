17ada1e99ac6b7b7ba580e38fcb1437030d8bc8a
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-28 22:41:50 +0900

    😊abc192-a_AC

---
+++ abc/192/a.cpp
@@ -0,0 +1,15 @@
+// 2024/05/28
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    int i;
+    for (i = x + 1; i % 100 != 0; i++)
+    {
+    }
+    cout << i - x << endl;
+}
