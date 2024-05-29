0ab8de1e666573322dee46cbe61d5bfd940c9a89
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-05-30 00:03:50 +0900

    😊abc191-a_AC

--- abc/191/a.cpp
+++ abc/191/a.cpp
@@ -1,5 +1,5 @@
 // 2024/05/29
-//
+// 自力AC
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,7 +7,7 @@ int main()
 {
     int v, t, s, d;
     cin >> v >> t >> s >> d;
-    if (v * (s - t) + v * t < d)
+    if (d < v * t || v * (s - t) + v * t < d)
     {
         cout << "Yes" << endl;
     }
