248e52a4e35afcc5f4ea07f6836fe05617f33a99
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-01 23:52:06 +0900

    😊abc178-a_AC

---
+++ abc/178/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/01
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x == 1)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        cout << "1" << endl;
+    }
+}
