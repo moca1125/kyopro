c7a2d2ea26f8a5b8fb13515fd0ee5db842ca732c
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-11 00:09:31 +0900

    😊abc170-a_AC

--- abc/170/a.cpp
+++ abc/170/a.cpp
@@ -1,13 +1,26 @@
 // 2024/06/10
 //
 #include <bits/stdc++.h>
-using namespace namepace;
+using namespace std;
 
 int main()
 {
-    vector<int> x(5);
+    vector<int> a(5);
+    bool ans = false;
     for (int i = 0; i < 5; i++)
     {
         cin >> a[i];
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        if (a[i] == 0)
+        {
+            cout << a[i + 1] - 1 << endl;
+            ans = true;
+        }
+    }
+    if (ans == false)
+    {
+        cout << a[3] + 1 << endl;
     }
 }
