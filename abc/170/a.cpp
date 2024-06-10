45f939e919f7f605f3cb7a8928988d69b888e5e0
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-10 23:59:47 +0900

    😊abc170-a_AC

---
+++ abc/170/a.cpp
@@ -0,0 +1,13 @@
+// 2024/06/10
+//
+#include <bits/stdc++.h>
+using namespace namepace;
+
+int main()
+{
+    vector<int> x(5);
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+}
