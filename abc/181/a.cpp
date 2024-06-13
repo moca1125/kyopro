cc9992a6c4f64a6cdfe54206a60729dfabde7720
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-13 23:47:46 +0900

    😊abc181-a_AC

---
+++ abc/181/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/13
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << "White" << endl;
+    }
+    else
+    {
+        cout << "Black" << endl;
+    }
+}
