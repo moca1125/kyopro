a57b03ad8666bb29f09beec56e19f096466d7a65
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-14 00:36:50 +0900

    😊abc174-a_AC

---
+++ abc/174/a.cpp
@@ -0,0 +1,14 @@
+// 2024/06/14
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (30 <= x)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
