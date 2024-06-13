11e11911117dd038d4e92a980830948bf0109377
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-14 00:49:33 +0900

    😊abc171-a_AC

---
+++ abc/171/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/14
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    if (a <= 'Z')
+    {
+        cout << "A" << endl;
+    }
+    else
+    {
+        cout << "a" << endl;
+    }
+}
