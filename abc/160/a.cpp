0079796e8b6192d4d1a533d227c079be2e6516c6
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-15 14:21:28 +0900

    😊abc160-a_AC

---
+++ abc/160/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/15
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (s[2] == s[3] && s[4] == s[5])
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
