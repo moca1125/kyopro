6f584644dcceca04db615b9dc24a6cc1ab217b78
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-17 17:35:02 +0900

    😊abc157-a_AC

---
+++ abc/157/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/17
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 == 0)
+    {
+        cout << n / 2 << endl;
+    }
+    else
+    {
+        cout << n / 2 + 1 << endl;
+    }
+}
