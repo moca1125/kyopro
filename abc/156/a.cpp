5a469bfd5868695e8e04be3995db9d2d834025fc
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-18 23:42:25 +0900

    😊abc156-a_AC

---
+++ abc/156/a.cpp
@@ -0,0 +1,20 @@
+// 2024/06/18
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, r;
+    cin >> n >> r;
+    int ans;
+    if (n < 10)
+    {
+        ans = r + 100 * (10 - n);
+    }
+    else
+    {
+        ans = r;
+    }
+    cout << ans << endl;
+}
