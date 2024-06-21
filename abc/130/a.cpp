505c5830c778d4bb366dfd1156bbc6e10cfcacb3
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-21 11:25:04 +0900

    😊abc130-a_AC

---
+++ abc/130/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/21
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, a;
+    cin >> x >> a;
+    if (x < a)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        cout << "10" << endl;
+    }
+}
