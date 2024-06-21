e00c3f8daff27fc06521eac8ee05d63aae349271
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-21 10:37:38 +0900

    😊abc212-a_AC

---
+++ abc/212/a.cpp
@@ -0,0 +1,22 @@
+// 2024/06/21
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (0 < a && b == 0)
+    {
+        cout << "Gold" << endl;
+    }
+    else if (a == 0 && 0 < b)
+    {
+        cout << "Silver" << endl;
+    }
+    else
+    {
+        cout << "Alloy" << endl;
+    }
+}
