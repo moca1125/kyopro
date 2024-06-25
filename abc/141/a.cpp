019a94a476d099255b77b4c3bd02b64fb0a17289
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-25 13:22:29 +0900

    😊abc141-a_AC

---
+++ abc/141/a.cpp
@@ -0,0 +1,22 @@
+// 2024/06/25
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if (s == "Sunny")
+    {
+        cout << "Cloudy" << endl;
+    }
+    else if (s == "Cloudy")
+    {
+        cout << "Rainy" << endl;
+    }
+    else
+    {
+        cout << "Sunny" << endl;
+    }
+}
