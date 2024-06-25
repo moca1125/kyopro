75733518a26a89bfdce6c4a318b6e1e6cfc174cc
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-25 13:18:36 +0900

    😊abc143-a_AC

---
+++ abc/143/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/25
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a > 2 * b)
+    {
+        cout << a - 2 * b << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+}
