034f32d4ae38c98586eaf2a39342d0c31a410154
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-25 13:10:07 +0900

    😊abc144-a_AC

---
+++ abc/144/a.cpp
@@ -0,0 +1,18 @@
+// 2024/06/25
+// 自力AC
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a <= 9 && b <= 9)
+    {
+        cout << a * b << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+}
