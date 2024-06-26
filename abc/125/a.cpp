33d89f9f49a6e6d164f7c6d8c933f450f413d710
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-26 20:04:02 +0900

    😊abc139-a_AC

---
+++ abc/125/a.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cassert>
+using namespace std;
+
+int main(){ 
+	int a,b,t;
+	cin>>a>>b>>t;
+	int ans=0;
+	for(int i=1;i<=t;i++){
+		if(i%a==0){
+		ans+=b;
+		}
+	}
+	cout<<ans<<endl;
+}
+
+
