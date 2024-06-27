b4738ca3e86a57a399887cfa5a890865e6d97d8e
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-27 21:36:55 +0900

    😊abc122-a_AC

---
+++ abc/122/a.cpp
@@ -0,0 +1,18 @@
+//2024/06/27
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+	char b;
+	cin>>b;
+	if(b=='A'){
+		cout<<"T"<<endl;
+	}else if(b=='T'){
+		cout<<"A"<<endl;
+	}else if(b=='C'){
+		cout<<"G"<<endl;
+	}else if(b=='G'){
+		cout<<"C"<<endl;
+	}
+}
