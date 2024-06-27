e223e7511453252ed8d7acd430455fbe3619bd27
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-27 18:24:30 +0900

    😊abc347-a_AC

---
+++ abc/347/a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,k;
+	cin>>n>>k;
+	int a;
+	for(int i=0;i<n;i++){
+		cin>>a;
+		if(a%k==0){
+			cout<<a/k<<" ";
+		}
+	}
+
+	cout<<endl;
+}
