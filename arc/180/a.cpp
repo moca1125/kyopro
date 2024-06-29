a066f4416cdff7cc495e68b7bcca5ef6a5e373dc
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-29 21:19:37 +0900

    😭arc180

---
+++ arc/180/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    long long num = 100000007;
+    string s;
+    string ans1;
+    cin >> s;
+    for (int i = 0; i < s.size() - 2; i++)
+    {
+        if (s[i] == 'A' && s[i + 1] == 'B' && s[i + 2] == 'A')
+        {
+            ans1 += 'A';
+        }
+        else
+        {
+            ans1 += s[i];
+        }
+    }
+    cout << ans1 << endl;
+}
