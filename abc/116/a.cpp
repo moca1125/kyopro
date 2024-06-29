248ab45cb52eebde444565f9592a26c92bb7b838
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-29 22:24:34 +0900

    😊abc116-a_AC

---
+++ abc/116/a.cpp
@@ -0,0 +1,14 @@
+//2024/06/29
+//
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+	vector<int> a(3);
+	cin>>a[0]>>a[1]>>a[2];
+	sort(a.begin(),a.end());
+	cout<<a[0]*a[1]/2<<endl;
+			}
+	
