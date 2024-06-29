c99b4ddf2ab1f190d0530f1d506cec50aafcf5ef
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-06-29 22:38:07 +0900

    😊abc115-a_AC

---
+++ abc/115/a.cpp
@@ -0,0 +1,18 @@
+//2024/06/29
+//自力AC
+#include<iostream>
+using namespace std;
+
+int main(){
+	int d;
+	cin>>d;
+	if(d==25){
+		cout<<"Christmas"<<endl;
+	}else if(d==24){
+		cout<<"Christmas Eve"<<endl;
+	}else if(d==23){
+		cout<<"Christmas Eve Eve"<<endl;
+	}else if(d==22){
+		cout<<"Christmas Eve Eve Eve"<<endl;
+	}
+}
