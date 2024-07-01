6178c5814fcdf1f500d9ff5886224e126a6c6468
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-01 14:41:11 +0900

    😊abc104-a_AC

---
+++ abc/104/a.cpp
@@ -0,0 +1,18 @@
+//2024/07/01
+//自力AC
+#include<iostream>
+using namespace std;
+
+int main(){
+	int r;
+	cin>>r;
+	if(r<1200){
+		cout<<"ABC"<<endl;
+	}else if(2800<=r){
+		cout<<"AGC"<<endl;
+	}else{
+		cout<<"ARC"<<endl;
+
+	}
+
+}
