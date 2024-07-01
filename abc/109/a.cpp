e8402633fc04dc9e45421375f55278736a6ab13e
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-01 13:09:07 +0900

    😊abc109-a_AC

---
+++ abc/109/a.cpp
@@ -0,0 +1,17 @@
+//2024/07/01
+//自力AC
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	if(a%2==0||b%2==0){
+		cout<<"No"<<endl;
+	}else{
+		cout<<"Yes"<<endl;
+	}
+
+
+
+}
