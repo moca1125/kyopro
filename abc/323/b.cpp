e85eb17d94b3998efef17b8d86289dce2baaf202
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-01 17:30:38 +0900

    😭abc323-b

--- abc/323/b.cpp
+++ abc/323/b.cpp
@@ -1,67 +1,28 @@
-// 2024/04/09
-// WA
-#include <bits/stdc++.h>
+//2024/07/01
+//
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<algorithm>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    vector<vector<char>> s(n, vector<char>(n));
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            cin >> s[j][i];
-        }
-    }
-    vector<int> check(n);
-    vector<int> check_2(n);
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            if (s[j][i] == 'o')
-            {
-                check[j] += 2;
-            }
-            else if (s[j][i] == '-')
-            {
-                check[j]++;
-            }
-        }
-    }
-    for (int j = 0; j < n; j++)
-    {
-        check_2[j] = check[j];
-    }
-    sort(check.rbegin(), check.rend());
-    vector<int> ans1;
-    vector<int> ans2;
-    int num = 0;
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            if (check[j] == check_2[i])
-            {
-                ans1.push_back(i + 1);
-            }
-        }
-    }
-    for (int j = 1; j < ans1.size(); j++)
-    {
-        for (int i = 0; i < ans2.size(); i++)
-        {
-            if (ans1[j] != ans2[i])
-            {
-                ans2.push_back(ans1[j]);
-            }
-        }
-    }
-    for (auto &ai : ans2)
-    {
-        cout << ai << " ";
-    }
-    cout << endl;
+int main(){
+	int n;
+	cin>>n;
+	vector<string> s(n);
+	for(int i=0;i<7;i++){
+		cin>>s[i];
+	}
+	vector<string> tmp=s;
+	sort(tmp.begin(),tmp.end());
+	for(int j=0;j<n;j++){
+		for(int i=0;i<n;i++){
+			if(tmp[j]==s[i]){
+				cout<<i<<" ";
+
+			}
+		}
+	}
+	cout<<endl;
 }
