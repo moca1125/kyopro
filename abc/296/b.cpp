68f568fb23a73b4cec170572a7475f2631d1d336
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-01 17:44:33 +0900

    😊abc296-a_AC

---
+++ abc/296/b.cpp
@@ -0,0 +1,23 @@
+//2024/07/01
+//
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+	vector<string> s(8);
+	for(int i=0;i<8;i++){
+		cin>>s[i];
+	}
+
+	for(int j=0;j<8;j++){
+		string tmp=s[j];
+		for(int i=0;i<8;i++){
+			if(tmp[i]=='*'){
+				cout<<char(i+'a');
+				cout<<8-j<<endl;
+			}
+		}	
+	}
+}
