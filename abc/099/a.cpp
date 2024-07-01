a5c6bcb2b89790bb639dbb2c683a4e4c2b7bba98
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-01 15:33:09 +0900

    😊abc099-a_AC

---
+++ abc/099/a.cpp
@@ -0,0 +1,17 @@
+//2024/07/01
+//自力AC
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	if(n<=999){
+
+		cout<<"ABC"<<endl;
+	}else{
+		cout<<"ABD"<<endl;
+	}
+
+
+}
---
+++ abc/106/a.cpp
@@ -0,0 +1,10 @@
+//2024/07/01
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cin>>a>>b;
+	cout<<a*b-b-a+1<<endl;
+}
