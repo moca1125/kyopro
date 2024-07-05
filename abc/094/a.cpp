f9e7ecd2c8df0715c33c6d6d43b2a697b358ae93
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-05 23:45:53 +0900

    😊abc094-a_AC

---
+++ abc/094/a.cpp
@@ -0,0 +1,15 @@
+//2024/07/05
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,x;
+	cin>>a>>b>>x;
+	if(a+b>=x&&a<=x){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+
+}
