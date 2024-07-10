47f427eebf2931b4fd93f54a54301d8442520b34
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-10 12:05:41 +0900

    😊abc078-a_AC

---
+++ abc/078/a.cpp
@@ -0,0 +1,16 @@
+//2024/07/10
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+	char a,b;
+	cin>>a>>b;
+	if(a>b){
+		cout<<">"<<endl;
+	}else if(a<b){
+		cout<<"<"<<endl;
+	}else{
+		cout<<"="<<endl;
+	}
+}
