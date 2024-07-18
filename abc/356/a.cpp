0b6552bce4e216709e2511a208f5ac38e0f08f44
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-19 00:46:12 +0900

    😭abc356-a_AC

--- abc/356/a.cpp
+++ abc/356/a.cpp
@@ -1,26 +1,21 @@
 //2024/07/18
 //
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int mian(){
+int main(){
 	int n,l,r;
 	cin>>n>>l>>r;
 	vector<int> a(n);
 	for(int i=0;i<n;i++){
 		a[i]=i+1;
 	}
-	int tmp;
+	for(int j=r;j>=l;j--){
+		a[n-j-1]=j;
+	}	
 	for(int i=0;i<n;i++){
-		if(i+1==l){
-			tmp=a[i];
-		}
-		if(i+1==r){
-			a[l-1]=a[i];
-			a[i]=tmp;
-		}
 		cout<<a[i]<<" ";
-		
 	}
 	cout<<endl;
 
