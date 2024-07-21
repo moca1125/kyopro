ba57614b3b8d7072a4a3ba4f66af956409985413
moca_1125_ <mokadayoganbarou1125improve@gmail.com>
2024-07-21 23:39:46 +0900

    😊abc362-a_AC

--- abc/362/a.cpp
+++ abc/362/a.cpp
@@ -1,1 +1,22 @@
+//2024/07/21
+//
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main(){
+	int r,g,b;
+	cin>>r>>g>>b;
+
+	string c;
+	cin>>c;
+
+	if(c=="Red"){
+		cout<<min(g,b)<<endl;
+	}else if(c=="Green"){
+		cout<<min(r,b)<<endl;
+	}else{
+		cout<<min(r,g)<<endl;
+	}
+
+}
