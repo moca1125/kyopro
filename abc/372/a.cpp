fc6e47e1956be7f85a8e0d708ab3c3361a7a6075
moca1125 <mokadayoganbarou1125improve@gmail.com>
2024-09-24 03:43:17 +0900

    😊abc372-a_AC

---
+++ abc/372/a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+	string s;
+	cin>>s;
+
+	for(int i=0;i<s.size();i++){
+
+		if(s[i]!='.'){
+			cout<<s[i];
+		}
+	}
+	cout<<endl;
+
+}
