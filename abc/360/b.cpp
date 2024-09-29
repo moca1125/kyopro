3a297a8ded7862b8383974b23b08744722ac69d6
moca1125 <mokadayoganbarou1125improve@gmail.com>
2024-09-29 13:20:08 +0900

    abc360-b_W_WA

---
+++ abc/360/b.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ string s,t;
+ cin>>s>>t;
+ string ans;
+ for(int w=1;w<s.size();w++){
+	for(int c=j;c<s.size();c+=w){
+	 ans+=s[i];
+ }
+if(ans==t){
+  cout<<"Yes"<<endl;
+  return 0;  
+}
+ans="";
+
+}
+cout<<"No"<<endl;
+}
+
