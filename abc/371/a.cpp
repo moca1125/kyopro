63ae77de33c793c4e463ee2ab49f50db0dad20c7
moca1125 <mokadayoganbarou1125improve@gmail.com>
2024-10-03 00:52:52 +0900

    😊abc371-a_AC

--- abc/371/a.cpp
+++ abc/371/a.cpp
@@ -1,9 +1,52 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
-	char a,b,c;
-	cin>>a>>b>>c;
+        vector<char> s(3);
+	for(int i=0;i<3;i++){
+		cin>>s[i];
+	}
+	vector<int> ans(3);
+	int num=0;
+	if(s[0]=='>'){
+		ans[0]++;
+		ans[1]--;
+	}else{
+		ans[1]++;
+		ans[0]--;
+	}if(s[1]=='>'){
+		ans[0]++;
+		ans[2]--;
+	}else{
+		
+		ans[0]--;
+		ans[2]++;
+	}
+	
+	if(s[2]=='>'){
 
+		ans[1]++;
+		ans[2]--;
+	}else{
+
+		ans[1]--;
+		ans[2]++;
+	}
+
+        vector<int> tmp=ans;
+	sort(tmp.begin(),tmp.end());
+	for(int i=0;i<3;i++){
+		if(tmp[1]==ans[i]){
+			if(i==0){
+				cout<<"A"<<endl;
+			}else if(i==1){
+				cout<<"B"<<endl;
+			}else{
+				cout<<"C"<<endl;
+			}
+		}
+	}
 
 }
